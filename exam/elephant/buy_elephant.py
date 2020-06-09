ae435c454a1d051365306c2bec3503877000e532
Diana <diana.art4@gmail.com>
2020-06-09 16:01:10 +0300

    Update buy_elephant.py

--- exam/elephant/buy_elephant.py
+++ exam/elephant/buy_elephant.py
@@ -1,3 +1,6 @@
+from io import StringIO
+
+
 START = "Приветствую тебя, путник, в моей сувенирной лавке. \n Могу предложить тебе купить слона."
 ARGUMENTS = ["Купи слона!", "Слону одиноко, купи слона!", "Я тебе скидку сделаю, купи слона!",
              "Ковер в подарок при покупке слона, купишь слона?", "Два слона по цене одного, купи слона!"]
@@ -15,6 +18,17 @@ def elephant():
     else:
         return "Вы остались без слона."
 
+
+def test_no(monkeypatch):
+    number_inputs = StringIO('No\nNo\nNo\nNo\nNo')
+    monkeypatch.setattr('sys.stdin', number_inputs)
+    assert elephant() == "Вы остались без слона."
+
+
+def test_yes(monkeypatch):
+    number_inputs = StringIO('yes\n')
+    monkeypatch.setattr('sys.stdin', number_inputs)
+    assert elephant() == "Поздравляю, теперь у вас есть слон!"
 
 if __name__ == '__main__':
     print(START)
